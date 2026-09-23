76ba16459241bac2aecee279ca5471c40d5dcef4
Diomande <ton_email@gmail.com>
2026-09-23 12:46:49 +0000

    Corrige la relation entreprise de l'utilisateur

--- comptes/models.py
+++ comptes/models.py
@@ -28,7 +28,7 @@ class Utilisateur(AbstractUser):
     
     entreprise = models.ForeignKey(
         "entreprises.Entreprise",
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         null=True,
         blank=True,
         related_name="utilisateurs",
